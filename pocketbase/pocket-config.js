a0d728bccdd085faaa7719a9ea7610f6555b4e6a
Erasmus001 <erasmusmensah00@gmail.com>
2024-03-14 01:41:50 +0000

    minor update

--- pocketbase/pocket-config.js
+++ pocketbase/pocket-config.js
@@ -1,4 +1,12 @@
 import Pocketbase from "pocketbase";
 
-export const pb = new Pocketbase('http://127.0.0.1:8090' || process.env.POCKETBASE_URL);
+let url;
+
+if (process.env.NODE_ENV === "production") {
+  url = process.env.POCKETBASE_URL;
+} else {
+  url = "http://127.0.0.1:8090";
+}
+
+export const pb = new Pocketbase(rul);
 pb.autoCancellation(false);
