49a5625db612338337b9cffe7bff281520af0b0f
Erasmus Mensah <erasmusmensah00@gmail.com>
2025-01-24 14:26:57 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,10 +4,7 @@ import { pb } from "./pocketbase/pocket-config";
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   console.log(request?.nextUrl);
-
-  console.log(pb.authStore);
-
-  return NextResponse.redirect(new URL("/signin", request.url));
+  // return NextResponse.redirect(new URL("/signin", request.url));
 }
 export const config = {
   matcher: ["/dashboard/:path"],
